8f7363dea5e8c84ae3ebef10fa36d57c82f64f7c
tam <tania_islas001@comunidad.unam.mx>
2025-04-07 23:54:38 -0600

    Mejoras en backButtom

--- app/src/main/java/com/example/noteferver/view/RegisterFragment.kt
+++ app/src/main/java/com/example/noteferver/view/RegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.noteferver.R
 import com.example.noteferver.databinding.RegisterFragmentBinding
 
 /**
@@ -14,21 +15,25 @@ import com.example.noteferver.databinding.RegisterFragmentBinding
 class RegisterFragment : Fragment() {
 
     private var _binding: RegisterFragmentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = RegisterFragmentBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {  // ¡Corregido! Usa override
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_signInFragment)
+
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
