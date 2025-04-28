d45f0803784e282c25161612d57da674d39d5355
tam <tania_islas001@comunidad.unam.mx>
2025-04-27 23:13:07 -0600

    ListActivity y Layouts

---
+++ app/src/main/java/com/example/noteferver/view/AddFragment.kt
@@ -0,0 +1,45 @@
+package com.example.noteferver.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.noteferver.databinding.FragmentAddBinding
+import com.example.noteferver.R
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class AddFragment : Fragment() {
+
+    private var _binding: FragmentAddBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btBack.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/noteferver/view/PendingFragment.kt
@@ -0,0 +1,45 @@
+package com.example.noteferver.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.noteferver.databinding.FragmentPendingsBinding
+import com.example.noteferver.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class PendingFragment : Fragment() {
+
+    private var _binding: FragmentPendingsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentPendingsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
